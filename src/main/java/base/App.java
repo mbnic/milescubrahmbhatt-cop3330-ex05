7a00d61f90f467406e9daf68a10a744c33b7acb6
nic <mbnic@icloud.com>
2021-06-05 23:43:32 -0400

    Initial commit

---
+++ src/main/java/base/App.java
@@ -0,0 +1,15 @@
+package base;
+import java.util.Scanner;
+
+public class App {
+    static Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        App myApp = new App();
+
+        Number x = new Number();
+        x.value = 69;
+
+        System.out.println(x.value);
+
+    }
+}
---
+++ src/main/java/base/Number.java
@@ -0,0 +1,5 @@
+package base;
+
+public class Number {
+    int value;
+}
